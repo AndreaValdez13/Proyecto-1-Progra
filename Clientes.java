3a7394a5b8fead1b71580f7a9acbac71447ea26a
AndreaValdez13 <47536477+AndreaValdez13@users.noreply.github.com>
2019-02-19 21:10:29 -0600

    Add files via upload

--- Clientes.java
+++ Clientes.java
@@ -6,94 +6,102 @@
 package Información;
 import LaTiendita.MenuPrincipal;
 import java.util.Scanner;
+
 /**
  *
- * @author WABS
+ * @author melis
  */
 public class Clientes {
     public Scanner in = new Scanner (System.in);
     int [] idCliente;
     String [] NCliente;
     String [] ApCliente;
-    String [] Nit;
+    int [] Nit;
     String [] Direccion;
     int opc;
     int i;
     
- public Clientes(){
+    public Clientes(){
+        this.idCliente = new int [10];
+        this.NCliente = new String [10];
+        this.ApCliente = new String [10];
+        this.Nit = new int[10];
+        this.Direccion = new String [10];
+        this.opc = 0;
+        this.i = 0;
+    }
 
-     this.idCliente = new int [10];
-     this.NCliente = new String [10];
-     this.ApCliente = new String [10];
-     this.Nit = new String [10];
-     this.Direccion = new String [10];
-     this.opc = 0;
-     this.i = 0;
- }
-
- public void MenuC(){
-     MenuPrincipal LaT;
-     LaT = new MenuPrincipal();
-       System.out.println("***MENU CLIENTES***");
-       System.out.println("1. Ingreso Clientes");
-       System.out.println("2. Consulta Clientes");
-       System.out.println("3. Regresar");
-       opc = in.nextInt();
+    public void MenuC(){
+        MenuPrincipal LaT;
+        LaT = new MenuPrincipal();
+        
+        System.out.println("\n***MENU CLIENTES***\n");
+        System.out.println("1. Ingreso Clientes");
+        System.out.println("2. Consulta Clientes");
+        System.out.println("3. Regresar");
+        opc = in.nextInt();
        
-       if(opc == 1){
-           ingresoClientes();
-       }else if (opc == 2){
-           ConsultaC();
-       }else if (opc == 3){
-           LaT.Menu();
-       }
-     
- }
+        if(opc == 1){
+            ingresoClientes();
+        }else if (opc == 2){
+            ConsultaC();
+        }else if (opc == 3){
+            LaT.Menu();
+        }    
+    }
  
-         public void ConsultaC(){
-                 MenuPrincipal LaT;
-                 LaT = new MenuPrincipal();
-             for (int p=0;p<=idCliente.length-1;p++){
-                 System.out.println("IdCliente: " + idCliente[p] );
+    public void ConsultaC(){
+        MenuPrincipal LaT;
+        LaT = new MenuPrincipal();
+        
+        for(int p =0; p < idCliente.length; p++){
+            System.out.println("\nIngrese Id del cliente que desea consultar");
+            i = in.nextInt();
+            if(idCliente[p]==i){
+                System.out.println("\nIdCliente: " + idCliente[p] );
                 System.out.println("---------------------\n");
-                 System.out.println("Nombre: " + NCliente[p]);
-                 System.out.println("---------------------\n");
-                 System.out.println("Precio: " + ApCliente [p]);
-                 System.out.println("---------------------\n");
-                 System.out.println("Stock: " + Nit [p]);
-                 System.out.println("---------------------\n");
-                 System.out.println("Categoria: " + Direccion [p]);
-                 System.out.println("---------------------\n");
-             }
-                LaT.Menu();
-           }
- 
+                System.out.println("Nombre: " + NCliente[p]);
+                System.out.println("---------------------\n");
+                System.out.println("Apellido: " + ApCliente[p]);
+                System.out.println("---------------------\n");
+                System.out.println("Nit: " + Nit[p]);
+                System.out.println("---------------------\n");
+                System.out.println("Direccion: " + Direccion[p]);
+                System.out.println("---------------------\n");
+                
+                System.out.println("\n ¿Desea realizar otra consulta? \n 1.Si \n 2.Regresar al menu");
+                opc = in.nextInt();
+                if(opc == 1){
+                    ConsultaC();
+                }else if(opc == 2){
+                    LaT.Menu();
+                }
+            }
+        }           
+    }
+                
     public void ingresoClientes(){
-         MenuPrincipal LaT;
-         LaT = new MenuPrincipal();
-        System.out.println("***INGRESO DE CLIENTES***\n");
+        MenuPrincipal LaT;
+                 
+        LaT = new MenuPrincipal();
+        System.out.println("\n***INGRESO DE CLIENTES***\n");
         
         do{
-         
-        System.out.println("Ingrese idCliente: ");
-        idCliente[i] = in.nextInt();
-        in.nextLine();
-         System.out.println("Ingrese Nombre de Cliente: ");
-        NCliente[i] = in.nextLine();
-        
-         System.out.println("Ingrese Apellido Cliente: ");
-        ApCliente[i] = in.nextLine();
-        
-         System.out.println("Ingrese Nit Cliente: ");
-        Nit[i] = in.nextLine();
-        
-        System.out.println("Ingrese Direccion de Cliente: ");
-        Direccion[i] = in.nextLine();
-        
-        System.out.println("***CLIENTE CREADO CON EXITO***\n");
-        i++;
-        System.out.println("Agregar otro Cliente?\n1.Si\n2.Consulta\n3.Regresar a Menu Principal\n");
-        opc = in.nextInt();
+            System.out.println("\nIngrese idCliente: ");
+            idCliente[i] = in.nextInt();
+            in.nextLine();
+            System.out.println("Ingrese Nombre de Cliente: ");
+            NCliente[i] = in.nextLine(); 
+            System.out.println("Ingrese Apellido Cliente: ");
+            ApCliente[i] = in.nextLine();
+            System.out.println("Ingrese Nit Cliente: ");
+            Nit[i] = in.nextInt();
+            System.out.println("Ingrese Direccion de Cliente: ");
+            Direccion[i] = in.nextLine(); 
+            System.out.println("\n***CLIENTE CREADO CON EXITO***\n");
+            i++;
+            System.out.println("\n¿Desea agregar otro Cliente?\n1.Si\n2.Consulta\n3.Regresar a Menu Principal\n");
+            opc = in.nextInt();
 
             if(opc == 1){
                 ingresoClientes();
@@ -102,6 +110,6 @@ public class Clientes {
             }else if (opc == 3){
              LaT.Menu();
             }
-             }while(i<10);
- }
+        }while(i<10);
+    }
 }
