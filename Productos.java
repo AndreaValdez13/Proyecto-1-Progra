4c6e4d7e573891e30fbc42ee215cb1f4ab45bf1f
AndreaValdez13 <47536477+AndreaValdez13@users.noreply.github.com>
2019-02-22 22:25:17 -0600

    Add files via upload

--- Productos.java
+++ Productos.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Información;
+package Informacion;
 import LaTiendita.MenuPrincipal;
 import java.util.Scanner;
 /**
@@ -19,7 +19,11 @@ public class Productos {
     String [] CatProducto;
     int opc;
     int i;
-    
+    int[] cantidad;
+    double[] subtotal;
+    int foo;
+    double [] Total;
+
     public Productos(){
         this.idProducto = new int [10];
         this.NProducto = new String [10];
@@ -28,6 +32,10 @@ public class Productos {
         this.CatProducto = new String [10];
         this.opc = 0;
         this.i = 0;
+        this.cantidad = new int [10];
+        this.subtotal = new double [10];
+        this.foo = 0;
+        this.Total = new double [10];
     }
 
     public void MenuP(){
@@ -62,10 +70,6 @@ public class Productos {
                 System.out.println("---------------------\n");
                 System.out.println("Precio: " + PrecioProducto [p]);
                 System.out.println("---------------------\n");
-                System.out.println("Stock: " + StockProducto [p]);
-                System.out.println("---------------------\n");
-                System.out.println("Categoria: " + CatProducto [p]);
-                System.out.println("---------------------\n");
                 
                 System.out.println("\n ¿Desea realizar otra consulta? \n 1.Si \n 2.Regresar al menu");
                 opc = in.nextInt();
@@ -74,7 +78,7 @@ public class Productos {
                 }else if(opc == 2){
                     LaT.Menu();
                 }
-            }             
+            }         
         }      
     }
  
@@ -91,11 +95,6 @@ public class Productos {
             NProducto[i] = in.nextLine();
             System.out.println("Ingrese Precio Producto: ");
             PrecioProducto[i] = in.nextDouble();
-            System.out.println("Ingrese Stock Producto: ");
-            StockProducto[i] = in.nextInt();
-            in.nextLine();
-            System.out.println("Ingrese Categoria de Producto: ");
-            CatProducto[i] = in.nextLine();
             System.out.println("***PRODUCTO CREADO CON EXITO***\n");
             i++;
             System.out.println("\n¿Desea agregar otro producto?\n1.Si\n2.Consulta\n3.Regresar a Menu Principal\n");
@@ -110,4 +109,48 @@ public class Productos {
             }
         }while(i<10);
     }
+    
+    public void DetalleFact(){
+        MenuPrincipal LaT;
+        LaT = new MenuPrincipal();
+        Clientes cl;
+        cl = new Clientes();
+        int opc, i, foo;
+        i = 0;
+        foo = 0;
+        opc = 0;
+        System.out.println("\nIntroduzca idProducto para facturacion:");
+        i = in.nextInt();
+        for(int p =0; p < idProducto.length; p++){
+            if(idProducto[p]==i){
+                System.out.println("\nIdProducto: " + idProducto[p] );
+                System.out.println("---------------------\n");
+                System.out.println("\nNombre: " + NProducto[p] );
+                System.out.println("---------------------\n");
+                System.out.println("\nPrecio: " + PrecioProducto[p] );
+                System.out.println("---------------------\n");
+            }
+        }
+        
+        for(int s = 0; s < 10; s++){
+            System.out.println("\nCantidad: ");
+            opc = in.nextInt();
+            cantidad [s] = opc;
+            subtotal[s] = cantidad [s] * PrecioProducto[s];
+            System.out.println("Total: " + subtotal[s]);
+          
+            System.out.println("\n¡Desea finalizar factura: Presione 0\n Continuar: Presione 1\n");
+            foo = in.nextInt();
+                if(foo == 0){
+                    for(int j = 0; j<subtotal.length;j++){
+                    Total[i] += subtotal[j];
+                }
+                    System.out.println("\nEl total de su factura es: Q" + Total[i]);
+                    LaT.Menu();
+                }else if(foo == 1){
+                    cl.EncaClientes();
+                }   
+        }
+        }
 }
+
